325ffb4b15bb64256a31e04ab11bbbdb3290b07a
Denys <tintalle@ip-172-31-4-198.eu-central-1.compute.internal>
2015-01-31 02:46:54 +0000

    fixed errors with not supplying libraries to the forked versions in doparallel

--- calcVol.R
+++ calcVol.R
@@ -1,23 +1,46 @@
 library(highfrequency)
 library(xts)
 library(doParallel)
-registerDoParallel(cores = 32)
+library(plyr)
+registerDoParallel(cores = 16)
 crncy = list("AUDUSD", "EURUSD", "GBPUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY", "USDNOK", "USDSEK")
-
+# crncy = "AUDUSD"
 calcRV = function(x){
-  RV = rKernelCov(x, kernel.type = "Epanechnikov", kernel.param=50, align.period = 5, makeReturns = TRUE)
+#   ewe
+  1
 }
 calcRVcrncy = function(crncy){
-  filename_xtsFX = paste("/data/", crncy, "_ticks", ".RData", sep = "")
-  load(filename_xtsFX)
+  print(paste("Start processing", crncy))
   
-  xtsFX_split = split.xts(xtsFX)
-  Values = unlist(llply(crncy, calcRV, .parallel = TRUE))
+  filename_xtsFX_split = paste("/data/", crncy, "_seconds_split", ".RData", sep = "")
+  load(filename_xtsFX_split)
+  gc()
+  
+  
+  
+#   ls_values = {foreach(i = 1:length(xtsFX_split)) %dopar% calcRV(xtsFX_split[[i]])}
+  ls_values = llply(xtsFX_split[1:10], calcRV, .parallel = TRUE, .inform = TRUE)
+  3460
+  
+  
+  
+  calcRV(xtsFX_split[3460])
+  
+  i = 3168
+
+  
+
+  Values = unlist(ls_values)
+  rm(ls_values); gc()
+#   Values = unlist(llply(xtsFX_split, calcRV, .parallel = TRUE))
   Dates = ldply(xtsFX_split, function(x) index(x[1]))[[1]]
+  rm(xtsFX_split);gc()
   xtsRV = xts(Values, Dates)
   filename_xtsRV = paste("/data/", crncy, "_RV", ".RData", sep = "")
-  save(xtsRV, filename_xtsRV)
+  save(xtsRV, file = filename_xtsRV)
   
+  print(paste("Finished processing", crncy))
+  gc()
 }
 
 l_ply(crncy, calcRVcrncy)
