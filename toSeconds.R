7a0db366d4acc5531eb776302197bbd7ef4fe4fc
Denys <tintalle@ip-172-31-4-198.eu-central-1.compute.internal>
2015-01-31 00:42:19 +0000

    transform dataset to 1sec frequency

---
+++ toSeconds.R
@@ -0,0 +1,32 @@
+library(highfrequency)
+library(xts)
+library(doParallel)
+library(plyr)
+registerDoParallel(cores = 15)
+crncy = list(#"AUDUSD", 
+  "EURUSD", "GBPUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY", "USDNOK", "USDSEK")
+# crncy = "AUDUSD"
+calcRV = function(x){
+  RV = rKernelCov(x, kernel.type = "Epanechnikov", kernel.param=50, align.period = 5, makeReturns = TRUE)
+}
+calcRVcrncy = function(crncy){
+  print(paste("Start processing", crncy))
+  
+  filename_xtsFX = paste("/data/", crncy, "_ticks", ".RData", sep = "")
+  load(filename_xtsFX)
+  
+  xtsFX_split1 = split.xts(xtsFX, f = "days")
+  rm(xtsFX); gc();
+  
+  xtsFX_split = llply(xtsFX_split1, 
+                      function(x) aggregatePrice(x, on = "seconds", k=1, 
+                                                 marketopen = "00:00:01", 
+                                                 marketclose = "23:59:59"),
+                      .parallel = TRUE)
+  rm(xtsFX_split1); gc();
+  
+  filename_xtsFX_split = paste("/data/", crncy, "_seconds_split", ".RData", sep = "")
+  save(xtsFX_split, file = filename_xtsFX_split)
+}
+
+l_ply(crncy, calcRVcrncy)
