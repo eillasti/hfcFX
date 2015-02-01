09b869e6b41e273535093a0d8e2f0c8bb95c04c3
Denys <tintalle@ip-172-31-0-67.eu-central-1.compute.internal>
2015-02-01 23:30:21 +0000

    fixed .packages=

--- toSeconds.R
+++ toSeconds.R
@@ -4,9 +4,10 @@ library(doParallel)
 library(plyr)
 registerDoParallel(cores = 16)
 crncy = list("AUDUSD", "EURUSD", "GBPUSD", "NZDUSD", "USDCAD", "USDCHF", "USDJPY", "USDNOK", "USDSEK")
-# crncy = "AUDUSD"
-calcRV = function(x){
-  RV = rKernelCov(x, kernel.type = "Epanechnikov", kernel.param=50, align.period = 5, makeReturns = TRUE)
+# crncy = "GBPUSD"
+toSeconds1 = function(x) {
+  aggregatePrice(x, on = "seconds", k=1, marketopen = "00:00:01",
+                 marketclose = "23:59:59")
 }
 calcRVcrncy = function(crncy){
   print(paste("Start processing", crncy))
@@ -18,10 +19,11 @@ calcRVcrncy = function(crncy){
   rm(xtsFX); gc();
   
   xtsFX_split = llply(xtsFX_split1, 
-                      function(x) aggregatePrice(x, on = "seconds", k=1, 
-                                                 marketopen = "00:00:01", 
-                                                 marketclose = "23:59:59"),
-                      .parallel = TRUE)
+                      toSeconds1,
+                      .parallel = TRUE,
+                      .paropts = list(".packages" = "highfrequency"))
+  if (any(unlist(llply(xtsFX_split, is.null)))) stop(paste("Something went wrong when processing", crncy))
+  
   rm(xtsFX_split1); gc();
   
   filename_xtsFX_split = paste("/data/", crncy, "_seconds_split", ".RData", sep = "")
